41010c550ee48c22d11f748e29821befb3f84afe
DeanSellas <dgsellas@gmail.com>
2019-11-12 15:13:48 -0600

    more fixing

--- app.py
+++ app.py
@@ -7,17 +7,16 @@ from src.guesser import Guesser
 from vendor.logger.logger import Logger
 
 
-Log = None
 def start(model="gpt2", interact=False, score=False, topK=10, text="", logpath=""):
     Log = Logger(filePath=logpath)
-    checks(model, topK)
+    checks(model, topK, Log)
     
     Log.Warn(text="Model: {} | Interact: {} | TopK: {} | Text: {}".format(
         model, interact, topK, text))
     guess = Guesser(model, interact, score, topK, Log)
     guess.start()
 
-def checks(model, topK):
+def checks(model, topK, Log):
     model_List = ["gpt2", "gpt2-medium", "gpt2-large"]
     if model not in model_List:
         Log.Error("Model Must be a value in {}".format(model_List), ValueError)
