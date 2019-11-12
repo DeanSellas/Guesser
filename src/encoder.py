645e4efb24f0b34e3fa005b174115dcf4fabca43
DeanSellas <dgsellas@gmail.com>
2019-11-12 15:48:07 -0600

    encoding

---
+++ src/encoder.py
@@ -0,0 +1,46 @@
+'''
+This class encodes text fed into it to be later used in the scoring algorithm.
+
+It picks random words inside the text and replaces with ${word} this will indicate to the scorer to score this word.
+
+TODO figure out best way to handle text 
+'''
+import random, time
+
+
+class Encoder():
+
+    def __init__(self, seed=0):
+        self.rand = random
+        if(seed < 1):
+            seed = int(time.time())
+        
+        self.rand.seed(seed)
+        
+        # calculates probability
+        self.prob = self.rand.random() * 0.25
+
+    def encode(self, text):
+        encodedText = ""
+        for word in text.split():
+            if self.rand.random() < self.prob:
+
+                # skip any non letter characters
+                if not ('a' <= word.lower() <= 'z'):
+                    continue
+                
+                word = "${"+self.clean(word)+"}"
+
+            encodedText += "{} ".format(word)
+        
+        return encodedText
+
+    def decode(self):
+        pass
+    
+    def clean(self, word):
+        out = ""
+        for c in word:
+            if 'a' <= c.lower() <= 'z':
+                out += c
+        return out
