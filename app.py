bf055bee528296c28b10b6252f66ae4ab20b0c19
DeanSellas <dgsellas@gmail.com>
2019-11-12 15:11:53 -0600

    small fix

--- app.py
+++ app.py
@@ -5,20 +5,9 @@ from fire import Fire
 from src.guesser import Guesser
 
 from vendor.logger.logger import Logger
-""" OLD SETUP CODE
-    model_117M = GPT2LanguageModel(model_name="gpt2")
-    logits = model_117M.predict("Joel is", "")
-    probabilities = torch.nn.functional.softmax(logits)
-
-    topk = 10
-
-    best_logits, best_indices = logits.topk(topk)
-    best_words = [model_117M[idx.item()] for idx in best_indices]
-    best_probabilities = probabilities[best_indices].tolist()
-    print(best_words)
-"""
 
 
+Log = None
 def start(model="gpt2", interact=False, score=False, topK=10, text="", logpath=""):
     Log = Logger(filePath=logpath)
     checks(model, topK)
