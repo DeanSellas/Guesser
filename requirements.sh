78a1cb135a34133414d9b8eeda004472d7121ff0
DeanSellas <dgsellas@gmail.com>
2019-12-05 18:32:45 -0600

    updates to requirements

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,9 @@
 # INSTALLS ALL REQUIREMENTS FOR THIS PROGRAM
 
-pip install -r requirements.txt
+pip3 install torch==1.3.1+cpu torchvision==0.4.2+cpu -f https://download.pytorch.org/whl/torch_stable.html
 
-pip install -r vendor/lmexplorer/requirements.txt
+pip3 install -r requirements.txt
 
-pip install -r vendor/transformers/requirements.txt
+pip3 install -r vendor/lmexplorer/requirements.txt
+
+pip3 install -r vendor/transformers/requirements.txt
